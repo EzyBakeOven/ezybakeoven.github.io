833305d5cf569e301eccd700888455755e1c194a
Matthew Hoy <matthew.hoy@govteams.gov.au>
2020-12-27 16:39:15 +1030

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,41 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from "./particle-header/header"
+
+export default function Layout({ children }) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Header />
+      <main>{children}</main>
+      <footer style={{
+        marginTop: `2rem`
+      }}>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer>
+    </ThemeProvider>
+  );
+}
---
+++ src/components/portrait/profile-pic.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `useStaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `useStaticQuery`: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+const ProfilePic = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "profile-pic.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
+  return <Img 
+    style={{
+      borderRadius: '80%',
+      border: '#1e1e1e .3em solid',
+      width: '30vh',
+      position: 'absolute',
+      top: '10vh'
+    }}
+    fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+export default ProfilePic
