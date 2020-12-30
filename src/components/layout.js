584f3b6f3fb2ef63e0ff1e488e0b991df1d1671c
Matthew Hoy <matthew.hoy@govteams.gov.au>
2020-12-30 11:48:36 +1030

    Added: framework preferance cards

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,36 +6,44 @@
  */
 
 import React from "react"
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import useMediaQuery from "@material-ui/core/useMediaQuery"
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
 import Header from "./particle-header/header"
 
 export default function Layout({ children }) {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
 
   const theme = React.useMemo(
     () =>
       createMuiTheme({
         palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
+          type: prefersDarkMode ? "dark" : "light",
         },
       }),
-    [prefersDarkMode],
-  );
+    [prefersDarkMode]
+  )
 
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline/>
+      <CssBaseline />
       <Header />
-      <main>{children}</main>
-      <footer style={{
-        marginTop: `2rem`
-      }}>
+      <main
+        style={{
+          margin: "1em",
+        }}
+      >
+        {children}
+      </main>
+      <footer
+        style={{
+          marginTop: `2rem`,
+        }}
+      >
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
       </footer>
     </ThemeProvider>
-  );
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import OutlinedCard from "../components/title-card/title-card"
 import CareerTimeline from "../components/career-timeline/career-timeline"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
+import PreferenceCard from "../components/preference-card/preference-card"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,6 +27,15 @@ export default function IndexPage() {
           </Grid>
           <Grid item xs={12} sm={12} md={9}>
             <CareerTimeline></CareerTimeline>
+          </Grid>
+        </Grid>
+
+        <Grid container justify="center" alignItems="center" spacing="3">
+          <Grid item xs={6} sm={6} md={6}>
+            <PreferenceCard framework="Frontend" primary="Angular" secondary="React"></PreferenceCard>
+          </Grid>
+          <Grid item xs={6} sm={6} md={6}>
+            <PreferenceCard framework="Backend" primary="Spring" secondary="Micronaut"></PreferenceCard>
           </Grid>
         </Grid>
       </div>
